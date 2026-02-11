231a7f70113a0c48efb8cbf84f175565a3e6522c
arthurgtp <diegogtz642@gmail.com>
2026-02-10 18:02:53 -0600

    Initial Commit

---
+++ InnSight/InnSightApp.swift
@@ -0,0 +1,17 @@
+//
+//  InnSightApp.swift
+//  InnSight
+//
+//  Created by Arturo Gutierrez on 10/02/26.
+//
+
+import SwiftUI
+
+@main
+struct InnSightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
